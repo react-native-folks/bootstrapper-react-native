a632ba3189558f905b1055b389a80d7d791a257d
Ignacio Abadie <iabadie@mahisoft.com>
2020-11-24 15:29:15 -0300

    Updated readme and firebase script

--- generators/app/templates/googleServicesConfig/firebaseFilesScript.sh
+++ generators/app/templates/googleServicesConfig/firebaseFilesScript.sh
@@ -21,14 +21,14 @@ fi
 if [ $2 = "ios" ]
 then
     if [ $1 = "com.kamino.develop" ]; then
-        cp -f GoogleServices/GoogleServiceDev-Info.plist GoogleService-Info.plist
+        cp -f GoogleServices/GoogleServiceDevelop-Info.plist GoogleService-Info.plist
     elif [ $1 = "com.kamino.staging" ]; then
         cp -f GoogleServices/GoogleServiceStaging-Info.plist GoogleService-Info.plist
     elif [ $1 = "com.kamino" ]; then
         cp -f GoogleServices/GoogleServiceProduction-Info.plist GoogleService-Info.plist
     else
         echo "Setting qa by default"
-        cp -f GoogleServices/GoogleServiceDev-Info.plist GoogleService-Info.plist
+        cp -f GoogleServices/GoogleServiceDevelop-Info.plist GoogleService-Info.plist
     fi
 fi
 
