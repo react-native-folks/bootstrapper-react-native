276a3b144339c364e6c11794cf303787969bbcbf
Ignacio Abadie <iabadie@mahisoft.com>
2021-02-04 20:27:14 -0300

    Fixed build and version numbers and added lint and jest test case

--- __test__/generator.test.js
+++ __test__/generator.test.js
@@ -3,13 +3,15 @@ const assert = require('yeoman-assert');
 const helpers = require('yeoman-test');
 const rimraf = require('rimraf');
 const { exec } = require('child_process');
+const util = require('util');
+const execPromise = util.promisify(exec);
 
 const cases = [
-  [1, ['Drawer', 'Tabs', 'Google Maps']], // drawer + tabs + google maps
-  [2, ['Login and SignUp', 'Onboarding']], // login + onboarding
-  [3, ['Login and SignUp']], // login
-  [4, ['Onboarding']], // onboarding
-  [5, []] // nada
+  [1, ['Drawer', 'Tabs', 'Google Maps']] // drawer + tabs + google maps
+  // [2, ['Login and SignUp', 'Onboarding']], // login + onboarding
+  // [3, ['Login and SignUp']], // login
+  // [4, ['Onboarding']], // onboarding
+  // [5, []] // nada
   // y despues  con recoil
   // [], // login + onboarding
   // [], // login
@@ -33,6 +35,22 @@ function buildAndroid(projDir) {
   });
 }
 
+function runLintAndTests(projDir) {
+  return new Promise(resolve => {
+    exec(
+      `cd ${projDir}/android && yarn lint && yarn test`,
+      (error, stdout, stderr) => {
+        resolve({
+          code: error && error.code ? error.code : 0,
+          error,
+          stdout,
+          stderr
+        });
+      }
+    );
+  });
+}
+
 describe('kamino-react-native:app', () => {
   const PROJECT_NAME = 'kaminorn';
   const TEMP_FOLDER = '../../tmp';
@@ -42,7 +60,7 @@ describe('kamino-react-native:app', () => {
     'Test generator, case %p with features %p',
     (caseId, features) => {
       const projectName = `${PROJECT_NAME}${caseId}`;
-
+      const projectDir = path.join(__dirname, `${TEMP_FOLDER}/${projectName}`);
       it.concurrent(
         `Create project ${projectName}`,
         () =>
@@ -60,21 +78,36 @@ describe('kamino-react-native:app', () => {
       );
 
       it(`Check package.json on ${projectName}`, () => {
-        assert.file(
-          path.join(__dirname, `${TEMP_FOLDER}/${projectName}/package.json`)
-        );
+        assert.file(`${projectDir}/package.json`);
       });
+
+      it(
+        'Project Lint and Tests should pass success for project ${projectName}',
+        async () => {
+          const result = await runLintAndTests(
+            path.join(__dirname, `${TEMP_FOLDER}/${projectName}`)
+          );
+          assert(result.code === 0);
+        },
+        GENERATOR_TIMEOUT
+      );
 
       it(
         `Android build must create corresponding apk for project ${projectName}`,
         async () => {
-          await buildAndroid(
-            path.join(__dirname, `${TEMP_FOLDER}/${projectName}`)
+          const { stdout } = await execPromise(
+            `cd ${projectDir} && git rev-list HEAD --count --merges --first-parent`
           );
+          const buildNumber = stdout ? Number(stdout) : 0;
+          const versionNumber =
+            require(`${projectDir}/package.json`).version ?? '0.0.1';
+          await buildAndroid(projectDir);
           assert.file(
             path.join(
-              __dirname,
-              `${TEMP_FOLDER}/${projectName}/android/app/build/outputs/apk/develop/debug/${PROJECT_NAME}${caseId}-0.0.1-1000-develop-debug.apk`
+              projectDir,
+              `/android/app/build/outputs/apk/develop/debug/${PROJECT_NAME}${caseId}-${versionNumber}-${
+                1000 + buildNumber
+              }-develop-debug.apk`
             )
           );
         },
