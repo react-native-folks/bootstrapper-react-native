73b88a683fed45ea59ce8ea43c1179d323f196c9
Ignacio Abadie <iabadie@mahisoft.com>
2021-03-09 12:24:42 -0300

    removed imported destructured hooks

--- generators/app/templates/src/hooks/theme.tsx
+++ generators/app/templates/src/hooks/theme.tsx
@@ -1,11 +1,11 @@
 import { AppTheme } from 'constants/colors';
 
-import React from 'react';
+import { createContext, useState } from 'react';
 import { useColorScheme } from 'react-native';
 
 export type ThemeProperty = 'light' | 'dark' | 'system' | string;
 
-export const ThemeContext = React.createContext({
+export const ThemeContext = createContext({
   changeTheme: (_newTheme: ThemeProperty) => {},
   appTheme: AppTheme.defaultTheme
 });
@@ -13,7 +13,7 @@ export const ThemeContext = React.createContext({
 export const useAppTheme = (
   initialStoredTheme: ThemeProperty
 ): [any, (theme: ThemeProperty) => void] => {
-  const [selectedTheme, setTheme] = React.useState(initialStoredTheme);
+  const [selectedTheme, setTheme] = useState(initialStoredTheme);
   const systemTheme = useColorScheme();
   let appTheme;
   if (selectedTheme === 'system') {
