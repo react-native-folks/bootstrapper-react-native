9a295a9dc212731e92aacf765a6a669e4ec8a6e2
Ignacio Abadie <iabadie@mahisoft.com>
2021-03-15 09:16:17 -0300

    stored selected theme on storage and handle cases on logout

--- generators/app/templates/src/services/preferences.ts
+++ generators/app/templates/src/services/preferences.ts
@@ -2,7 +2,7 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const THEME_KEY = '@Preferences:appTheme';
 
-export const setAppThemePreference = (value: string) =>
+export const setAppThemePreference = (value: 'dark' | 'light' | 'system') =>
   AsyncStorage.setItem(THEME_KEY, value || 'system');
 export const getAppThemePreference = () => AsyncStorage.getItem(THEME_KEY);
 
