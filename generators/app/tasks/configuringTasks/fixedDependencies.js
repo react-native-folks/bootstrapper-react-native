f6dcb1945e3f4b449814be1af362afc82cdae61e
Ignacio Abadie <iabadie@mahisoft.com>
2021-03-31 11:57:55 -0300

    moved dependencies object to a const and created function to get version

--- generators/app/tasks/configuringTasks/fixedDependencies.js
+++ generators/app/tasks/configuringTasks/fixedDependencies.js
@@ -1,51 +1,29 @@
 module.exports.FIXED_RN_VERSION = '0.63.3';
 
 module.exports.getFixedDependencies = function getFixedDependencies() {
-  return {
-    REACT_NAVIGATION_NATIVE: `@react-navigation/native${
-      this.options.latest ? '' : '@^5.9.3'
-    }`,
-    REACT_NATIVAGITION_STACK: `@react-navigation/stack${
-      this.options.latest ? '' : '@^5.14.3'
-    }`,
-    REACT_NAVIGATION_DRAWER: `@react-navigation/drawer${
-      this.options.latest ? '' : '@^5.12.4'
-    }`,
-    REACT_NAVIGATION_TABS: `@react-navigation/bottom-tabs${
-      this.options.latest ? '' : '@^5.11.8'
-    }`,
-    REACT_NATIVE_REANIMATED: `react-native-reanimated${
-      this.options.latest ? '' : '@^2.0.1'
-    }`,
-    REACT_NATIVE_SAFE_AREA_CONTEXT: `react-native-safe-area-context${
-      this.options.latest ? '' : '@^3.2.0'
-    }`,
-    REACT_NATIVE_SCREENS: `react-native-screens${
-      this.options.latest ? '' : '@^2.18.1'
-    }`,
-    REACT_NATIVE_MASKED_VIEW: `@react-native-community/masked-view${
-      this.options.latest ? '' : '@^0.1.10'
-    }`,
-    REACT_NATIVE_GESTURE_HANDLER: `react-native-gesture-handler${
-      this.options.latest ? '' : '@^1.10.3'
-    }`,
-    REACT_NATIVE_FIREBASE_ANALYTICS: `@react-native-firebase/analytics${
-      this.options.latest ? '' : '@^11.1.2'
-    }`,
-    REACT_NATIVE_FIREBASE_APP: `@react-native-firebase/app${
-      this.options.latest ? '' : '@^11.1.2'
-    }`,
-    REACT_NATIVE_FIREBASE_CRASHLYTICS: `@react-native-firebase/crashlytics${
-      this.options.latest ? '' : '@^11.1.2'
-    }`,
-    REACT_NATIVE_FIREBASE_PERF: `@react-native-firebase/perf${
-      this.options.latest ? '' : '@^11.1.2'
-    }`,
-    REACT_NATIVE_FIREBASE_MESSAGING: `@react-native-firebase/messaging${
-      this.options.latest ? '' : '@^11.1.2'
-    }`,
-    REACT_NATIVE_CONFIG: `react-native-config${
-      this.options.latest ? '' : '@^1.4.2'
-    }`
-  };
+  const fixedDeps = {};
+  for (const key of Object.keys(DEPENDENCIES)) {
+    const lib = DEPENDENCIES[key];
+    fixedDeps[key] = this.options.latest ? lib : lib.split('@^')[0];
+  }
+  return fixedDeps;
+};
+
+const DEPENDENCIES = {
+  REACT_NAVIGATION_NATIVE: '@react-navigation/native@^5.9.3',
+  REACT_NATIVAGITION_STACK: '@react-navigation/stack@^5.14.3',
+  REACT_NAVIGATION_DRAWER: '@react-navigation/drawer@^5.12.4',
+  REACT_NAVIGATION_TABS: '@react-navigation/bottom-tabs@^5.11.8',
+  REACT_NATIVE_REANIMATED: 'react-native-reanimated@^1.0.0',
+  REACT_NATIVE_SAFE_AREA_CONTEXT: 'react-native-safe-area-context@^3.2.0',
+  REACT_NATIVE_SCREENS: 'react-native-screens@^2.18.1',
+  REACT_NATIVE_MASKED_VIEW: '@react-native-community/masked-view@^0.1.10',
+  REACT_NATIVE_GESTURE_HANDLER: 'react-native-gesture-handler@^1.10.3',
+  REACT_NATIVE_FIREBASE_ANALYTICS: '@react-native-firebase/analytics@^11.1.2',
+  REACT_NATIVE_FIREBASE_APP: '@react-native-firebase/app@^11.1.2',
+  REACT_NATIVE_FIREBASE_CRASHLYTICS:
+    '@react-native-firebase/crashlytics@^11.1.2',
+  REACT_NATIVE_FIREBASE_PERF: '@react-native-firebase/perf@^11.1.2',
+  REACT_NATIVE_FIREBASE_MESSAGING: '@react-native-firebase/messaging@^11.1.2',
+  REACT_NATIVE_CONFIG: 'react-native-config@^1.4.2'
 };
