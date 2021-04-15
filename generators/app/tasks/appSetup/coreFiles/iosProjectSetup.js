96ddc280749d292980e1182c62a623dd0e1470e9
Ignacio Abadie <iabadie@mahisoft.com>
2021-04-15 13:44:46 -0300

    removed flipper fixed dependenciees

--- generators/app/tasks/appSetup/coreFiles/iosProjectSetup.js
+++ generators/app/tasks/appSetup/coreFiles/iosProjectSetup.js
@@ -1,12 +1,5 @@
 const createSchemes = require('./multipleEnvIos/createSchemes');
 
 module.exports = function fixBundleIndentifier() {
-  // Forced flipper sdk to version 74 since actually RN initialize projects on 54
-  const podfileContent = this.fs.read(`${this.projectName}/ios/Podfile`);
-  const fixedPodfileContent = podfileContent.replace(
-    'use_flipper!',
-    "use_flipper!({ 'Flipper-Folly' => '~> 2.2.0' })"
-  );
-  this.fs.write(`${this.projectName}/ios/Podfile`, fixedPodfileContent);
   createSchemes.bind(this)();
 };
