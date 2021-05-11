2fbbc4c74cea084b56176d885597373b4095e7ec
Walter Folini <wfolini@mahisoft.com>
2021-05-11 15:53:35 +0200

    Enable hermes on iOS

--- generators/app/tasks/appSetup/coreFiles/iosProjectSetup.js
+++ generators/app/tasks/appSetup/coreFiles/iosProjectSetup.js
@@ -1,5 +1,14 @@
 const createSchemes = require('./multipleEnvIos/createSchemes');
 
 module.exports = function fixBundleIndentifier() {
+  // Enable hermes
+  let podfileContent = this.fs.read(`${this.projectName}/ios/Podfile`);
+  podfileContent = podfileContent.replace(
+    ':hermes_enabled => false',
+    ':hermes_enabled => true'
+  );
+
+  this.fs.write(`${this.projectName}/ios/Podfile`, podfileContent);
+
   createSchemes.bind(this)();
 };
